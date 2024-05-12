803d8091db8280e3bf8b5f3fc26059924562f6d3
BPplays <andymalbp@gmail.com>
2024-05-12 00:29:51 -0700

    changed to async

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from api_info import key
 import signal
 import sys
 from lang import iso_languages
+import asyncio
+import aiohttp
+import time
 
 def append_non_duplicates(arr1, arr2):
-    unique_elements = set(arr1)
-    for item in arr2:
-        if item not in unique_elements:
-            arr1.append(item)
-            unique_elements.add(item)
+	unique_elements = set(arr1)
+	for item in arr2:
+		if item not in unique_elements:
+			arr1.append(item)
+			unique_elements.add(item)
 
 
 def print_lang():
@@ -22,10 +25,10 @@ def print_lang():
 
 
 def signal_handler(sig, frame):
-    print('Exiting gracefully...')
-    print_lang()
-    # Perform cleanup operations here if needed
-    sys.exit(0)
+	print('Exiting gracefully...')
+	print_lang()
+	# Perform cleanup operations here if needed
+	sys.exit(0)
 
 # Register the signal handler for Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
@@ -36,26 +39,27 @@ def is_valid_language_code(input_code):
 			return True
 	return False
 
-def check_language(game_id, api_key, type):
+async def check_language(game_id, api_key, type):
 	url = f'https://www.steamgriddb.com/api/v2/{type}/game/{game_id}'
 	headers = {'Authorization': f'Bearer {api_key}'}
-	response = requests.get(url, headers=headers)
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url, headers=headers) as response:
+			if response.status == 200:
+				data = await response.json()
+				iso_languages_matched = []
+				non_iso_languages = []
 
-	if response.status_code == 200:
-		data = response.json()
-		iso_languages_matched = []
-		non_iso_languages = []
+				for game_data in data['data']:
+					language = game_data['language']
+					if is_valid_language_code(language):
+						iso_languages_matched.append(language)
+					else:
+						non_iso_languages.append(language)
 
-		for game_data in data['data']:
-			language = game_data['language']
-			if is_valid_language_code(language):
-				iso_languages_matched.append(language)
+				return iso_languages_matched, non_iso_languages
 			else:
-				non_iso_languages.append(language)
+				return None, None
 
-		return iso_languages_matched, non_iso_languages
-	else:
-		return None, None
 
 
 
@@ -76,27 +80,50 @@ non_iso_languages = []
 
 to_disp_full = 0
 
-while len(iso_languages_matched) < 184:
-	for type in apiopts:
-		iso_languages_matched_tmp, non_iso_languages_tmp = check_language(game_id, api_key, type)
+requests_count = 0
 
-		if (iso_languages_matched_tmp is not None) and (len(iso_languages_matched_tmp) != 0):
-			to_disp_full -= 1
-			append_non_duplicates(iso_languages_matched, iso_languages_matched_tmp)
-			print(iso_languages_matched_tmp)
-	
-		if (non_iso_languages_tmp is not None) and (len(non_iso_languages_tmp) != 0):
-			to_disp_full -= 1
-			append_non_duplicates(non_iso_languages, non_iso_languages_tmp)
-			print(non_iso_languages_tmp)
-	
+REQUESTS_INTERVAL = 5
+start_time = time.time()
+
+batch_size = 80
+async def main():
+	global game_id, to_disp_full, requests_count, start_time
+	while len(iso_languages_matched) < 184:
+		tasks = []
+		
+		i = batch_size
+		while i > 0:
+			for type in apiopts:
+				task = asyncio.create_task(check_language(game_id, api_key, type))
+				tasks.append(task)
+			game_id -= 1
+			i -=1
+
+		results = await asyncio.gather(*tasks)
+
+		for iso_languages_matched_tmp, non_iso_languages_tmp in results:
+			if iso_languages_matched_tmp and len(iso_languages_matched_tmp) != 0:
+				to_disp_full -= 1
+				append_non_duplicates(iso_languages_matched, iso_languages_matched_tmp)
+				print(iso_languages_matched_tmp)
+
+			if non_iso_languages_tmp and len(non_iso_languages_tmp) != 0:
+				to_disp_full -= 1
+				append_non_duplicates(non_iso_languages, non_iso_languages_tmp)
+				print(non_iso_languages_tmp)
+
 		if to_disp_full < 0:
 			to_disp_full = 50
 			print_lang()
+		requests_count += len(tasks)
 
-	game_id -= 1
+		
+		if time.time() - start_time >= REQUESTS_INTERVAL:
+			print(f"Requests per second: {requests_count / REQUESTS_INTERVAL}")
+			requests_count = 0
+			start_time = time.time()
  
-
+asyncio.run(main())
 	
 	# test = game_id
 	# if test +5 >= game_id:
